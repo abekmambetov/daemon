3aced75c8fc2a23921f678c05194096bdb420fda
abekmambetov <43209750+abekmambetov@users.noreply.github.com>
2021-08-31 21:23:24 +0400

    Update daemon.php

--- daemon.php
+++ daemon.php
@@ -1,6 +1,6 @@
 <?php
 while(true) {
-	$appUrl = "http://localhost/"; // url приложения
+    $appUrl = "http://localhost/"; // url приложения
     $interval = 60; // интервал запроса в секундах
 
     file_get_contents($appUrl);
